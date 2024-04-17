e0238ac6e69c23799ae5cd20f0ae8f93f52f30f7
Narae Gwon <rnjs021031@gmail.com>
2024-04-17 13:58:44 +0900

    20240417 ch06 주석 추가

--- ch06_view/src/main/java/com/example/ch6_view/MainActivity.kt
+++ ch06_view/src/main/java/com/example/ch6_view/MainActivity.kt
@@ -3,9 +3,10 @@ package com.example.ch6_view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+// 액티비티: 화면을 구성하는 컴포넌트
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) // xml로 작성한 뷰를 화면에 출력
     }
 }
