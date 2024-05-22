78ceefa3f908f44894f532cb7c13f71ddfd9bc41
Narae Gwon <rnjs021031@gmail.com>
2024-05-22 17:53:05 +0900

    20240522 ch21 firestorage

--- ch18_image/src/main/java/com/example/ch18_image/AddActivity.kt
+++ ch18_image/src/main/java/com/example/ch18_image/AddActivity.kt
@@ -1,19 +1,42 @@
 package com.example.ch18_image
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.example.ch18_image.databinding.ActivityAddBinding
 import java.text.SimpleDateFormat
 
 class AddActivity : AppCompatActivity() {
     lateinit var  binding: ActivityAddBinding
+    lateinit var uri: Uri
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.tvId.text = MyApplication.email
+
+        val requestLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode === android.app.Activity.RESULT_OK) {
+                binding.addImageView.visibility = View.VISIBLE
+                Glide.with(applicationContext)
+                    .load(it.data?.data)
+                    .override(250, 200)
+                    .into(binding.addImageView)
+                uri = it.data?.data!!
+            }
+        }
+        binding.uploadButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*")
+            requestLauncher.launch(intent)
+        }
 
         binding.saveButton.setOnClickListener {
             if (binding.input.text.isNotEmpty()) {
@@ -29,6 +52,7 @@ class AddActivity : AppCompatActivity() {
                     .add(data)
                     .addOnSuccessListener {
                         Toast.makeText(this, "데이터 저장 성공", Toast.LENGTH_SHORT).show()
+                        uploadImage(it.id)
                         finish()
                     }
                     .addOnFailureListener {
@@ -37,6 +61,17 @@ class AddActivity : AppCompatActivity() {
             } else {
                 Toast.makeText(this, "한줄평 먼저 입력해주세요..", Toast.LENGTH_SHORT).show()
             }
+        }
+    }
+
+    fun uploadImage(docId: String) {
+        val imageRef = MyApplication.storage.reference.child("images/${docId}.jpg")
+
+        val uploadTask = imageRef.putFile(uri)
+        uploadTask.addOnSuccessListener {
+            Toast.makeText(this, "이미지 업로드 성공", Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener {
+            Toast.makeText(this, "이미지 업로드 실패", Toast.LENGTH_SHORT).show()
         }
     }
 }
--- ch18_image/src/main/java/com/example/ch18_image/BoardActivity.kt
+++ ch18_image/src/main/java/com/example/ch18_image/BoardActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ch18_image.databinding.ActivityBoardBinding
+import com.google.firebase.firestore.Query
 
 class BoardActivity : AppCompatActivity() {
     lateinit var binding: ActivityBoardBinding
@@ -19,6 +21,29 @@ class BoardActivity : AppCompatActivity() {
             } else {
                 Toast.makeText(this, "인증을 먼저 해주세요..", Toast.LENGTH_SHORT).show()
             }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if (MyApplication.checkAuth()) {
+            MyApplication.db.collection("comments")
+                .orderBy("date_time", Query.Direction.DESCENDING)
+                .get()
+                .addOnSuccessListener { result ->
+                    val itemList = mutableListOf<ItemData>()
+                    for (document in result) {
+                        val item = document.toObject(ItemData::class.java)
+                        item.docId = document.id
+                        itemList.add(item)
+                    }
+                    binding.recyclerView.layoutManager = LinearLayoutManager(this)
+                    binding.recyclerView.adapter = BoardAdapter(this, itemList)
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "서버 데이터 획득 실패", Toast.LENGTH_SHORT).show()
+                }
         }
     }
 }
